a4d844f0c60f9251c2870426ef1904409585e339
net <net@DESKTOP-37IA93U>
2020-01-30 10:15:54 +0100

    Agregar archivos de proyecto.

---
+++ BookFilmProject/Models/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookFilmProject.Models
+{
+    public class Libro
+    {
+        public int LibroID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Theme { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public string Music { get; set; }
+        public bool PendienteVista { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ BookFilmProject/Models/Pelicula.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookFilmProject.Models
+{
+    public class Pelicula
+    {
+        public int FilmID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Theme { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public string Music { get; set; }
+        public bool PendienteVista { get; set; }
+        public DateTime Date { get; set; }
+        
+
+        public int LibroID { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
+
+
+
+
+
+    
